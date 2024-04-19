eac0bcdc4c3ed6f97c0a47dd3964626627359458
annemarie1310 <khajindar.pratik@gmail.com>
2024-04-19 11:52:58 +0530

    dczxcd

---
+++ src/Main.java
@@ -0,0 +1,121 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //variables :-
+/*
+        int number =55;
+        float pi = 3.10F;
+        long phone =5555555555L;
+        double pie =31.2222222;
+        char ch ='a';
+        String name="pranay";
+        boolean  rain =true;
+*/
+
+// taking input form user in java .
+/*
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("enter your name , age , salary");
+
+        // string input below :-
+        String name=myObj.nextLine();
+
+         number input below :-
+        int age =myObj.nextInt();
+        double salary = myObj.nextDouble();
+*/
+
+        // below are some string functions :-
+ /*
+        System.out.println("Name : "+name.toLowerCase());
+        System.out.println("Name : "+name.toUpperCase());
+        System.out.println(name.contains("pra"));
+        System.out.println(name.charAt(2));
+        System.out.println(name.endsWith("ay"));
+        System.out.println(name.indexOf("a"));
+*/
+
+    // below are some inbuilt maths functions
+/*
+        int a=70,b=8;
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.sqrt(4));       // returns the square root of number passed in() .
+        System.out.println(Math.abs(-22));      // returns a positive value even if we enter negative value
+        System.out.println(Math.random()*5);      // generates random number from 0 to 1 .
+        System.out.println(Math.PI);
+*/
+/*
+  int num;
+  Scanner week =new Scanner(System.in);
+  System.out.println("enter a number like 1 for monday, 2 for tuesday etc .. ");
+  int choice =week.nextInt();
+
+  switch (choice) {
+
+      case 1:
+          System.out.println("its monday");
+          break;
+      case 2:
+          System.out.println("its tuesday");
+          break;
+      case 3:
+          System.out.println("its wednesday");
+          break;
+      case 4:
+          System.out.println("its thursday");
+          break;
+      case 5:
+          System.out.println("its friday");
+          break;
+      case 6:
+          System.out.println("its saturday");
+          break;
+      case 7:
+          System.out.println("its sunday");
+          break;
+      default:
+          System.out.println("OOPs  !! Wrong choice  !!!!!!");
+          break;
+  }
+*/
+/*
+//        Arrays Below
+
+// 1D array below
+int [] marks ={1,2,3,4,5};
+
+//        2D array below
+        int [][] marks = {{1,2,3},{4,5,6}};
+        System.out.println(marks[1][2]);
+
+         // string array below
+String [] cars ={"Maruti","Ford","Merc","BMW"} ;
+        for (String value:cars)                            // this is for each loop ..
+        {
+            System.out.println(value);
+        }
+    */
+/*
+        // Try and catch
+        String [] cars ={"Maruti","Ford","Merc","BMW"} ;
+
+        try                                        // this will try to run the code if error it will ..
+        {
+            System.out.println(cars[5]);
+        }
+        catch (Exception e)                      // catch here in the variable 'e' and ..
+        {
+            System.out.println(e);              // and it will print the error here .. and continue code from here .
+        }
+
+        System.out.println("hello");
+
+*/
+
+        
+
+    }
+}
